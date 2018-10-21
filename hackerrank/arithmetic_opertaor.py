94b02cc985b6fef7b1a04f17ac0e28dba7fa5351
Hemant B <hemantb@redbytes.in>
2018-10-22 01:37:03 +0530

    arithmetic operator

---
+++ hackerrank/arithmetic_opertaor.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?h_r=next-challenge&h_v=zen
+
+number1 = int(raw_input("Enter first number: "))
+number2 = int(raw_input("Enter second number: "))
+
+print number1 + number2
+print number1 - number2
+print number1 * number2
