12f75230cf3ad4cc2c8cefc8a5b9b927c9fc4cfc
Hemant B <hemantb@redbytes.in>
2018-10-22 01:22:39 +0530

    first 2 programs done

---
+++ hackerrank/if_else.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/py-if-else/problem
+
+n = int(raw_input('Enter number'))
+
+if not n % 2 == 0:
+    print 'Weird'
+
+elif n % 2 == 0 and n in range(2, 5):
+    print 'Not Weird'
+
+elif n % 2 == 0 and n in range(6, 20):
+    print 'Weird'
+
+elif n > 20:
+    print 'Not Weird'
