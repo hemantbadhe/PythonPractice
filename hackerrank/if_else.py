57511473e65a41eaf9c024623bd4539b4c8b9306
Hemant B <hemantb@redbytes.in>
2018-10-22 01:29:36 +0530

    first 2 programs done

--- hackerrank/if_else.py
+++ hackerrank/if_else.py
@@ -8,7 +8,7 @@ if not n % 2 == 0:
 elif n % 2 == 0 and n in range(2, 5):
     print 'Not Weird'
 
-elif n % 2 == 0 and n in range(6, 20):
+elif n % 2 == 0 and n in range(6, 21):
     print 'Weird'
 
 elif n > 20:
