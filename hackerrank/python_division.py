2c8305a156da0b10ac3f6d577a971d1c37abc8db
Hemant B <hemantb@redbytes.in>
2018-10-22 01:42:09 +0530

    fourth completed

---
+++ hackerrank/python_division.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-division/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+number1 = int(raw_input("Enter number1: "))
+number2 = int(raw_input("Enter number2: "))
+
+print int(number1 / number2)
+print float(number1 / number2)
